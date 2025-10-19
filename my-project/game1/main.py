47aa73e052609db02c1a9c7879e71ad2cd62b405
BakaYaroslav <jaroslavbaka25@gmail.com>
2025-10-19 22:24:50 +0300

    animation player

--- my-project/game1/main.py
+++ my-project/game1/main.py
@@ -4,11 +4,21 @@ pygame.init()
 screen = pygame.display.set_mode((600, 360)) # Create a window of size 800x600
 pygame.display.set_caption("My Game") # Set the window title
 
-pygame.display.set_icon(pygame.image.load('my-project/game1/images/pig.png')) # Set and load the window icon
-pig = pygame.image.load('my-project/game1/images/pig.png') # Load an image
-pig = pygame.transform.scale(pig, (50, 50)) # Scale the image to 100x100 pixels
+pygame.display.set_icon(pygame.image.load('my-project/game1/images/player.goes straight1.png')) # Set and load the window icon
 backgroud = pygame.image.load('my-project/game1/images/bg.png')
-
+walk_right = [pygame.image.load('my-project/game1/images/player.goes right1.png'),
+              pygame.image.load('my-project/game1/images/player.goes right2.png'),
+              pygame.image.load('my-project/game1/images/player.goes right3.png'),
+              pygame.image.load('my-project/game1/images/player.goes right4.png'),
+              ]
+walk_left = [pygame.image.load('my-project/game1/images/player.goes left1.png'),
+             pygame.image.load('my-project/game1/images/player.goes left2.png'),
+             pygame.image.load('my-project/game1/images/player.goes left3.png'),
+             pygame.image.load('my-project/game1/images/player.goes left4.png'),
+             ]
+player_anim_count = 0
+walk_right = [pygame.transform.scale(img, (50, 50)) for img in walk_right] # Scale images to 50x50
+walk_left = [pygame.transform.scale(img, (50, 50)) for img in walk_left] # Scale images to 50x50
 
 running = True
 
@@ -16,7 +26,12 @@ while running: # бесконечный цикл игры, что бы окно 
     
     
     screen.blit(backgroud, (0, 0)) # Draw the background image onto the screen at position (0, 0)
-    screen.blit(pig, (50, 210)) # Draw the image onto the screen at position (100, 100)
+    screen.blit(walk_right[player_anim_count], (50,260)) # Draw the image onto the screen at position (100, 100)
+    
+    if player_anim_count == 3:
+        player_anim_count = 0
+    else:
+        player_anim_count += 1
     
     
     pygame.display.update() # Update the display
@@ -25,6 +40,7 @@ while running: # бесконечный цикл игры, что бы окно 
         if event.type == pygame.QUIT: # проверка на закрытие окна
             running = False
             pygame.quit() # Exit the program
+            
        
                  
                     
