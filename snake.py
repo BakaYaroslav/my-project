af274cec133837e6c23c5f11ea25834579a06495
BakaYaroslav <jaroslavbaka25@gmail.com>
2025-11-02 15:58:40 +0200

    изменение физики змейки и добавление с счетчиком

--- snake.py
+++ snake.py
@@ -11,6 +11,7 @@ pygame.display.set_caption("Snake Game")
 
 game_end = True
 clock = pygame.time.Clock() #таймер фпс
+font = pygame.font.SysFont("Arial", 24, bold=True)
 
 
 colors = { 
@@ -21,9 +22,9 @@ colors = {
     "apple": (255, 0, 0,)    # красный
 }
 snake_pos = {
-    "x": 320,
+    "x": 220,
     "y": 240,
-    "x_change": 0,
+    "x_change": 10,
     "y_change": 0,
 }
 
@@ -31,9 +32,12 @@ snake_speed = 10
 
 snake_size = (10, 10)
 
-snake_tails = 10
 
-snake_tails = []
+snake_tails = [
+    [snake_pos["x"] - 10, snake_pos["y"]],
+    [snake_pos["x"] - 20, snake_pos["y"]],
+    [snake_pos["x"] - 30, snake_pos["y"]]
+]
 # рандомный спавн для яблочек
 apple_pos = {
     'x': round(random.randrange(0, width - snake_size[0]) / 10)* 10,
@@ -43,18 +47,12 @@ apple_pos = {
 
 apple_size = (10, 10) 
 apple_eaten = 0
-# начальные елементы для хвоста
-
-snake_tails.append([snake_pos["x"]+10, snake_pos["y"]])
-snake_tails.append([snake_pos["x"]+20, snake_pos["y"]])
-snake_tails.append([snake_pos["x"]+30, snake_pos["y"]])
-
 
 while game_end:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_end = False        
-        elif event.type == pygame.KEYDOWN:  # 
+        elif event.type == pygame.KEYDOWN:  
             if event.key == pygame.K_a and snake_pos["x_change"] == 0:
                 snake_pos["x_change"] = -snake_speed # смещение влево на 10 пикселей заданных ранее
                 snake_pos["y_change"] = 0
@@ -75,22 +73,27 @@ while game_end:
                 pygame.draw.rect(display, colors["bg1"], [x, y, 10, 10])
             else:
                 pygame.draw.rect(display, colors["bg2"], [x, y, 10, 10])
-        # движение змейки
+    # движение змейки
     
     ilx = snake_pos["x"]
     ily = snake_pos["y"]
              
     snake_pos["x"] += snake_pos["x_change"]
     snake_pos["y"] += snake_pos["y_change"]
-    
-    for i in range(len(snake_tails) -1, 0, -1): # обновление хвоста с начала до конца
-        snake_tails[i][0] = snake_tails[i-1][0] # x-координата i-го сегмента хвоста
-        snake_tails[i][1] = snake_tails[i-1][1] # y-координата i-го сегмента хвоста
-    if snake_tails:
+    if len(snake_tails) > 0:
+        for i in range(len(snake_tails) -1, 0, -1): # обновление хвоста с начала до конца
+            snake_tails[i][0] = snake_tails[i-1][0] # x-координата i-го сегмента хвоста
+            snake_tails[i][1] = snake_tails[i-1][1] # y-координата i-го сегмента хвоста
+        if snake_tails:
         
-        snake_tails[0][0] = ilx
-        snake_tails[0][1] = ily
- 
+            snake_tails[0][0] = ilx
+            snake_tails[0][1] = ily
+    if snake_tails:
+        last_tail = snake_tails[-1]
+    else:
+        last_tail = [snake_pos["x"], snake_pos["y"]]
+        snake_tails.append(last_tail)
+    # рисуем хвост змеи
     for t in snake_tails:
         pygame.draw.rect(display, colors["snake_tall"], [
             t[0],
@@ -110,12 +113,15 @@ while game_end:
         apple_pos["y"],
         apple_size[0],
         apple_size[1]])
+    text = (f"Apples: {apple_eaten}   L: {len(snake_tails)}")
+    text_surface = font.render(text, True, (245,245,245)) # белый цвет текста
+    display.blit(text_surface, (10, 10))
     
     # проверка змейка на той ли позиции что и яблоко
     if(snake_pos["x"] == apple_pos['x'] and
         snake_pos["y"] == apple_pos['y']):
         apple_eaten += 1
-        snake_tails.append([apple_pos["x"], apple_pos["y"]]) 
+        snake_tails.append([last_tail[0], last_tail[1]]) 
         
         apple_pos = {
         'x': round(random.randrange(0, width - snake_size[0]) / 10) * 10,
@@ -123,16 +129,17 @@ while game_end:
         
         }
     
-    for i in range(len(snake_tails)): # обновление хвоста с начала до конца
+    '''for i in range(len(snake_tails)): # обновление хвоста с начала до конца
         if(snake_pos["x"] + snake_pos["x_change"] == snake_tails[i][0]
            and snake_pos ['y'] + snake_pos["y_change"] == snake_tails[i][1]):
             snake_tails = snake_tails[:i]
+            break'''
+    for t in snake_tails:
+        if(snake_pos["x"] == t[0] and snake_pos["y"] == t[1]):
+            game_end = False
             break
         
-    
-
-        
-    # телепорт змеи если она покидает екран
+    ''' # телепорт змеи если она покидает екран
     if(snake_pos["x"] < -snake_size[0]):
         snake_pos['x'] = width - 10
         
@@ -143,9 +150,12 @@ while game_end:
         snake_pos['y'] = height -10
         
     elif(snake_pos["y"] > height -10):
-        snake_pos["y"] = 0
+        snake_pos["y"] = 0'''
+    # конец игры если змейка выходит за границы экрана
+    if(snake_pos["x"] < 0 or snake_pos["x"] > width -10 or
+        snake_pos["y"] < 0 or snake_pos["y"] > height -10):
+        game_end = False
         
     pygame.display.update()
     clock.tick(30)  #ограничение фпс
 pygame.quit()
-quit()
